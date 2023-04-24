d9be9638150309ad9a53388c147cdd93c22f6759
peterwebster1998 <peter.webster1998@gmail.com>
2023-04-24 15:41:04 -0400

    Initial Commit

---
+++ Motiv/MotivApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotivApp.swift
+//  Motiv
+//
+//  Created by Peter Webster on 4/24/23.
+//
+
+import SwiftUI
+
+@main
+struct MotivApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
